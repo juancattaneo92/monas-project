cd244a72a8a1275fefec4e2a4960a444203bfbe6
Juan <juancattaneo92@gmail.com>
2021-04-01 12:44:53 -0700

    menu items reducers created

---
+++ frontend/reducers/MenuItemReducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_ALL_MENU_ITEMS, RECEIVE_MENU_ITEM } 
+from '../actions/MenuItemActions';
+
+const menuItemsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state)
+  switch (action.type) {
+    case RECEIVE_ALL_MENU_ITEMS:
+      newState = action.menuItems;
+      return newState;
+    case RECEIVE_MENU_ITEM:
+      newState[action.menuItem.id] = action.menuItem
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default menuItemsReducer;
