6a77df3c41171ebc0e48f542eb62711530086890
Juan <juancattaneo92@gmail.com>
2021-04-01 12:30:55 -0700

    menu item actions created

---
+++ frontend/actions/MenuItemActions.js
@@ -0,0 +1,29 @@
+import * as MenuItemAPIUtil from '../util/MenuItemApiUtil';
+
+export const RECEIVE_ALL_MENU_ITEMS = 'RECEIVE_ALL_MENU_ITEMS';
+export const RECEIVE_MENU_ITEM = 'RECEIVE_MENU_ITEM';
+
+export const receiveAllMenuItems = (menuItems) => {
+  return {
+    type: RECEIVE_ALL_MENU_ITEMS,
+    menuItems
+  }
+}
+export const receiveMenuItem = (menuItem) => {
+  return {
+    type: RECEIVE_MENU_ITEM,
+    menuItem,
+  }
+}
+
+//Thunk Actions
+
+export const fetchMenuItems = () => (dispatch) => {
+  return MenuItemAPIUtil.fetchMenuItems()
+    .then( result => dispatch(receiveAllMenuItems(result)))
+}
+
+export const fetchMenuItem = (menuItemId) => (dispatch) => {
+  return MenuItemAPIUtil.fetchProduct(menuItemId)
+  .then( result => dispatch(receiveProduct(result)))
+};
