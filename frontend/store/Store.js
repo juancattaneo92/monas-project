f7c138f7fae2d0d4865d8362059664cd301af379
Juan <juancattaneo92@gmail.com>
2021-04-06 12:06:55 -0700

    store updated

--- frontend/store/Store.js
+++ frontend/store/Store.js
@@ -3,6 +3,15 @@ import RootReducer from '../reducers/RootReducer';
 import logger from "redux-logger"
 import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+
 const configureStore = (preloadedState = {}) => {
   return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
 }
