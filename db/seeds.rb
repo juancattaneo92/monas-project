e509b9693f6b5a7e05356edcd063f4edc71f23f1
Juan <juancattaneo92@gmail.com>
2021-04-30 23:11:02 -0700

    added burgers to data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+MenuItem.destroy_all
+
+item1 = MenuItem.create(name: 'Monas Burgers', description: 'Amazing Burger', price:'15.00', ingredients:'Brioche Bun, Sauted Onions, BBq, Jack Cheese')
+item2 = MenuItem.create(name: 'Classic Burger', description: 'Amazing Burger', price:'15.00', ingredients:'Brioche Bun, Onion, Tomatoe, lettuce')
+item3 = MenuItem.create(name: 'Drunken Burger', description: 'Amazing Burger', price:'15.00', ingredients:'Brioche Bun, Crispy Onion, Drunken Sauce, Blue Cheese')
+item4 = MenuItem.create(name: 'Screw Diet Burger', description: 'Amazing Burger', price:'15.00', ingredients:'Brioche Bun, Fried Egg, Crispy Onion, Jack Cheese')
+item5 = MenuItem.create(name: 'Cali Burger', description: 'Amazing Burger', price:'15.00', ingredients:'Brioche Bun, Jack Cheese, Avocado, Sprouts')
--- frontend/components/MenuItem/MenuItemIndex.jsx
+++ frontend/components/MenuItem/MenuItemIndex.jsx
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react'
-// import { fetchMenuItems } from '../../actions/MenuItemActions'
-import { fetchMenuItems } from '../../util/MenuItemApiUtil'
+import { fetchMenuItems } from '../../actions/MenuItemActions'
+// import { fetchMenuItems } from '../../util/MenuItemApiUtil'
 
-export default function MenuItemIndex(props){
+
+export default function MenuItemIndex( props ){
   const [menuItems, setMenuItems] = useState();
+  const[isLoading] = useState(true)
   
   useEffect(() => {
-    items.then(menuItems => {
-      let items = fetchMenuItems(menuItems)
-        setMenuItems(items)
-      })
-  }, [menuItems])
+    fetchMenuItems()
+    console.log("working")
+    }, [])
+
 
   return(
     <div className="menu-index-section">
       <div className="wrapper-menu-index">
+        {/* { menuItems.map(item => {
+          item.name
+        }) */}
 
-        {menuItems.map(item=> <li key={item.id}>{item.name}</li>)}
+        
       </div>
     </div>
   )
