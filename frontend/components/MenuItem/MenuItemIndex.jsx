8d5829c3751fcb0cd8f0dbaa9ae1c6e44edfb3d6
paulobocanegra <paulobocanegra@gmail.com>
2021-05-18 10:11:01 -0700

    new aws images

--- frontend/components/MenuItem/MenuItemIndex.jsx
+++ frontend/components/MenuItem/MenuItemIndex.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react'
 
-export default function MenuItemIndex(){
+export default function MenuItemIndex() {
   const [items, setItems] = useState([]);
 
-    const getItems = async () => {
-      const response = await fetch('api/menu_items')
-      const items = await response.json()
-        setItems(items)
-    }
+  const getItems = async () => {
+    const response = await fetch('api/menu_items')
+    const items = await response.json()
+    setItems(items)
+  }
 
-    useEffect(()=>{
-      getItems()
-    }, [])
+  useEffect(() => {
+    getItems()
+  }, [])
 
   // const categories = [Burgers, Salads, Starters, Fries, Un-Burgers, Beverages, Sides]
 
-  return(
+  return (
     <div>
       <div></div>
       <section className="menu-index-section">
@@ -23,23 +23,23 @@ export default function MenuItemIndex(){
           {items.map(item => {
             return (
               <ul className="menu-item-index" key={item.id}>
-                  <img src={'https://happilyunprocessed.com/wp-content/uploads/2018/03/Juicest-Burger-Everfeature.jpg.jpg'} alt=""/>
-                  <div className="menu-details">
-                    <div className="menu-name">
-                      <li>{item.name}</li>
-                      <li>$ {item.price}</li>
-                    </div>
-                    <li>{item.description}</li>
-                    <button className='action-button menu-items'>Take me Home</button>
+                <img src={item.photo} alt=""/>
+                <div className="menu-details">
+                  <div className="menu-name">
+                    <li>{item.name}</li>
+                    <li>$ {item.price}</li>
                   </div>
+                  <li>{item.description}</li>
+                  <button className='action-button menu-items'>Take me Home</button>
+                </div>
               </ul>
             )
           })}
-          
+
         </div>
         <div className="divider"></div>
         <div className="shopping-cart-index">
-            <h1 className="current-order-title">Ready To Order?</h1>
+          <h1 className="current-order-title">Ready To Order?</h1>
         </div>
       </section>
     </div>
