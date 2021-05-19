7e8eed72167edf68a3b76f0e537222642b9eefed
Juan <juancattaneo92@gmail.com>
2021-05-19 11:35:10 -0700

    grassfed full screen fixed

--- frontend/components/Grassfed/Grassfed.jsx
+++ frontend/components/Grassfed/Grassfed.jsx
@@ -6,22 +6,21 @@ export class Grassfed extends Component {
 
     render() {
         
-//         return (
-//             <section className="feature-1">
-//                 <div className="feature-wrapper">
-//                     <div data-aos='zoom-in' data-aos-duration='500' className="grass-fed-blurb-container">
-//                         <h2 className="feature-title">Why Grassfed Matters</h2>
-//                     <p className="grass-fed-blurb" > 
-
-// Mona's Burgers are made with Non-Antibiotic, 100% grass-fed beef from cows only from Niman Ranch, never frozen, delivered every single day...</p>
-//                         <button data-aos='zoom-out' data-aos-duration='500' data-aos-delay="100" className="action-button learn-more-button">Learn More</button>
-//                 </div>
-//                 <div className="feature-image-container">
-//                     <img data-aos='zoom-in' data-aos-duration='500' className="feature-1-image"src={ window.feature_1_url} alt=""/>
-//                     </div>
-//                     </div>
-//             </section>
-//         )
+        return (
+            <section className="feature-1">
+                <div className="feature-wrapper">
+                    <div data-aos='zoom-in' data-aos-duration='500' className="grass-fed-blurb-container">
+                        <h2 className="feature-title">Why Grassfed Matters</h2>
+                         <p className="grass-fed-blurb" > 
+                        Mona's Burgers are made with Non-Antibiotic, 100% grass-fed beef from cows only from Niman Ranch, never frozen, delivered every single day...</p>
+                        <button data-aos='zoom-out' data-aos-duration='500' data-aos-delay="100" className="action-button learn-more-button">Learn More</button>
+                    </div>
+                    <div className="feature-image-container">
+                        <img data-aos='zoom-in' data-aos-duration='500' className="feature-1-image"src={ window.feature_1_url} alt=""/>
+                    </div>
+                </div>
+            </section>
+        )
     }
 }
 
