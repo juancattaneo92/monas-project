e20c3226ac00d8d1faab29e7eab50c27f0468c37
Juan <juancattaneo92@gmail.com>
2021-05-20 17:28:54 -0700

    menu-items page add categories

--- frontend/components/MenuItem/MenuItemIndex.jsx
+++ frontend/components/MenuItem/MenuItemIndex.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 export default function MenuItemIndex() {
   const [items, setItems] = useState([]);
+  const [category, setCategory] = useState([]);
 
   const getItems = async () => {
     const response = await fetch('api/menu_items')
@@ -9,18 +10,33 @@ export default function MenuItemIndex() {
     setItems(items)
   }
 
+  const sortByCategories = () => {
+
+  }
+ 
   useEffect(() => {
     getItems()
   }, [])
 
-  // const categories = [Burgers, Salads, Starters, Fries, Un-Burgers, Beverages, Sides]
+  const categories = ["Burgers", "Salads", "Starters", "Fries", "Un-Burgers", "Beverages", "Sides"]
 
   return (
     <div>
-      {/* <div>Burgers, Salads, Starters, Fries, Un-Burgers, Beverages, Sides</div> */}
-      <section className="menu-index-section m-t">
+      <div className="menu-navBar-section m-t">
+        <div className="menu-navBar-container">
+          <div className="menu-category">{categories.map(cat => {
+            return (
+              <ul className="menu-single-category" >{cat}</ul>
+            )
+          })}
+          </div>
+          <input className="menu-searchBar" type="text" placeholder="What are you looking for?"></input>
+        </div>
+      </div>
+      <section className="menu-index-section">
         <div className="menu-index-container">
           <div className="wrapper-menu-index">
+            
             {items.map(item => {
               return (
                 <ul className="menu-item-index" key={item.id}>
