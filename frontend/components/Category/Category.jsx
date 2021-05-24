6ea7c28901910d9ece69c52ad8f14c93ca5f1aa1
Juan <juancattaneo92@gmail.com>
2021-05-24 11:43:31 -0700

    url fixed but it needs to be fixed to auto refresh

--- frontend/components/Category/Category.jsx
+++ frontend/components/Category/Category.jsx
@@ -5,13 +5,13 @@ export const Category = () => {
 
   return (
     <div className="menu-category">
-            <Link to='menu-items/burgers' className="menu-single-category" >Burgers</Link>
-            <Link to='menu-items/salad' className="menu-single-category" >Salads</Link>
-            <Link to='menu-items/starters' className="menu-single-category" >Starters</Link>
-            <Link to='menu-items/fries' className="menu-single-category" >Fries</Link>
-            <Link to='menu-items/beverage' className="menu-single-category" >Beverages</Link>
-            <Link to='menu-items/milkshake' className="menu-single-category" >Milshakes</Link>
-            <Link to='menu-items/un-burger' className="menu-single-category" >Un-Burgers</Link> 
+            <Link to='/menu-items/burgers' className="menu-single-category" >Burgers</Link>
+            <Link to='/menu-items/salad' className="menu-single-category" >Salads</Link>
+            <Link to='/menu-items/starters' className="menu-single-category" >Starters</Link>
+            <Link to='/menu-items/fries' className="menu-single-category" >Fries</Link>
+            <Link to='/menu-items/beverage' className="menu-single-category" >Beverages</Link>
+            <Link to='/menu-items/milkshake' className="menu-single-category" >Milshakes</Link>
+            <Link to='/menu-items/un-burger' className="menu-single-category" >Un-Burgers</Link> 
     </div>
   )
 }
--- frontend/components/MenuItem/MenuItemIndex.jsx
+++ frontend/components/MenuItem/MenuItemIndex.jsx
@@ -3,31 +3,75 @@ import { Link } from 'react-router-dom'
 import Category from '../Category/Category';
 
 export default function MenuItemIndex() {
-  const [category, setCategory] = useState([]);
+  const [items, setItems] = useState([]);
+  // const [category, setCategory] = useState("")
+  const [loading, setLoading] = useState(true)
 
-  const getCategory = async () => {
+
+  // Message:
+  // This is not ready!!! the url is fixed but once I add dependency [items] 
+  // it updates the request (component did update) however, we fall into a 
+  // infinity loop. We need a cleanup, a return with a call back into the return 
+  // inside the useEffect. However we get a push history issue because we are 
+  // getting categories from the same url in theory (api/menu-items)
+  //(Warning: Hash history cannot PUSH the same path; a new entry will not be added to the history stack)
+  // If we hit the buttons for burgers or salads ..etc it doesnt go, only goes 
+  // if we refresh the page. 
+  // What we need to do is find another way to get the categories
+  // I was trying to get categories from line 7 and 21 to 28, however I cant
+  // get the current category 
+
+
+  // const getCategory = async () => {
+  //   const response = await fetch('api/menu_items')
+  //   const category = await response.json()
+  //   let categories = category.filter( (product) => {
+  //     return product.category
+  //   })
+  //   setCategory(category)
+  //   console.log(categories)
+  // }
+ 
+  const getItems = async () => {
     const category = window.location.hash.split("/")[2]
     console.log(category)
+
     const response = await fetch(`api/menu-items/${category}`)
     const items = await response.json()
-    setCategory(items)
+    setItems(items)
+    console.log(items)
   }
-   useEffect(() => {
-    getCategory()
+  useEffect(() => {
+    let mounted = true
+    console.log("useEffect")
+    // getCategory()
+    getItems()
+    .then(()=> {
+      if(mounted){
+        setLoading(false)
+      }
+    })
+    return () => {
+      mounted = false
+    }
   }, [])
+console.log("render")
 
 return (
+  <div>
+    { loading ? <h1 className="m-t">Loading...</h1> : 
     <div className="menu-index">
-      <div className="menu-navBar-section">
+
+     <div className="menu-navBar-section">
         <div className="menu-navBar-container">
-            {/* <Category /> */}
+            <Category />
           <input className="menu-searchBar" type="text" placeholder="What are you looking for?"></input>
         </div>
       </div>
       <section className="menu-index-section">
         <div className="menu-index-container">
           <div className="wrapper-menu-index">
-            {category.map(item => {
+            {items.map(item => {
               return (
                 <ul className="menu-item-index" key={item.id}>
                   <img src={item.photo} alt=""/>
@@ -49,6 +93,7 @@ return (
         </div>
       </section>
     </div>
+  }</div>
   )
 }
 
